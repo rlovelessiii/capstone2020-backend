56cb5d9ffda3a7e84e2d193c96a988600882aed5
rloveles <rloveles@uncc.edu>
2020-04-12 14:58:04 -0400

    Updated all routes for the parent portal route, still needs testing

--- routes/portal.js
+++ routes/portal.js
@@ -87,30 +87,139 @@ createViewsTable()
 createWatchedTable()
 createSavedTable()
 
+const handleError = (res, errorNum) => {
+  switch (errorNum) {
+    case 19: return res.status(409).send('User information already entered')
+    case 'views': return res.status(404).send('User view history not found!')
+    case 'watched': return res.status(404).send('User watch history not found')
+    case 'saved': return res.status(404).send('User save history not found')
+    default: return res.status(500).send('Server Error')
+  }
+}
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+
+  const user_id = req.body['userId']
+
+  findViewsById(user_id, (error, views) => {
+    if (error) handleError(res, error.errno)
+    else findWatchedById(user_id, (error, watched) => {
+      if (error) handleError(res, error.errno)
+      else findSavedById(user_id, (error, saved) => {
+        if (error) handleError(res, error.errno)
+        else res.status(200).send({ views, watched, saved })
+      })
+    })
+  })
+})
+
 router.get('/views', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+
+  findViewsById(user_id, (error, views) => {
+    if (error) handleError(res, error.errno)
+    else if (!views) handleError (res, 'watched')
+    else res.status(200).send(views)
+  })
 })
 
 router.post('/views', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+  const title_type = req.body['titleType']
+  const title_id = req.body['titleId']
+
+  addView(user_id, title_type, title_id, (error) => {
+    if (error) handleError(res, error.errno)
+    else findViewsById(user_id, (error, views) => {
+      if (error) handleError(res, error.errno)
+      else if (!views) handleError(res, 'views')
+      else res.status(200).send(views)
+    })
+  })
 })
 
 router.get('/watched', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+
+  findWatchedById(user_id, (error, watched) => {
+    if (error) handleError(res, error.errno)
+    else if (!watched) handleError (res, 'watched')
+    else res.status(200).send(watched)
+  })
 })
 
 router.post('/watched', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+  const title_type = req.body['titleType']
+  const title_id = req.body['titleId']
+  const season = req.body['season']
+  const episode = req.body['episode']
+  const rating = req.body['rating']
+
+  addWatched(user_id, title_type, title_id, season, episode, rating, (error) => {
+    if (error) handleError(res, error.errno)
+    else findWatchedById(user_id, (error, watched) => {
+      if (error) handleError(res, error.errno)
+      else if (!watched) handleError(res, 'watched')
+      else res.status(200).send(watched)
+    })
+  })
+
 })
 
 router.get('/saved', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+
+  findSavedById(user_id, (error, saved) => {
+    if (error) handleError(res, error.errno)
+    else if (!saved) handleError (res, 'saved')
+    else res.status(200).send(saved)
+  })
 })
 
 router.post('/saved', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+  const save_type = req.body['saveType']
+  const title_type = req.body['titleType']
+  const title_id = req.body['titleId']
+
+  addSaved(user_id, save_type, title_type, title_id, (error) => {
+    if (error) handleError(res, error.errno)
+    else findSavedById(user_id, (error, saved) => {
+      if (error) handleError(res, error.errno)
+      else if (!saved) handleError(res, 'saved')
+      else res.status(200).send(saved)
+    })
+  })
 })
 
-router.delete('saved', (req, res) => {
+router.delete('/saved', (req, res) => {
   console.log(req.body)
+
+  const user_id = req.body['userId']
+  const save_type = req.body['saveType']
+  const title_type = req.body['titleType']
+  const title_id = req.body['titleId']
+
+  removeSaved(user_id, save_type, title_type, title_id, (error) => {
+    if (error) handleError(res, error.errno)
+    else findSavedById(user_id, (error, saved) => {
+      if (error) handleError(res, error.errno)
+      else if (!saved) handleError(res, 'saved')
+      else res.status(200).send(saved)
+    })
+  })
 })
